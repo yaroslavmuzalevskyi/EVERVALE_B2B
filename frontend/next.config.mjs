1a10eeb2e68185343a342f34d1041e38d43d0619
Yaroslav <166743450+yaroslavmuzalevskyi@users.noreply.github.com>
2026-02-16 13:28:38 +0100

    update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,9 @@
 const nextConfig = {
   /* config options here */
   output: "export",
-  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -13,7 +13,7 @@ const variants = {
   primary:
     `btn-text w-auto h-[44px] bg-pr_y text-pr_dg rounded-full px-5 py-2 shadow-[0_10px_20px_rgba(0,0,0,0.18)] hover:-translate-y-0.5 hover:shadow-[0_14px_26px_rgba(0,0,0,0.22)] ${motionClass}`,
   header:
-    `btn-text w-auto h-[32px] bg-pr_y text-pr_dg rounded-full px-3 py-1 shadow-[0_8px_16px_rgba(0,0,0,0.16)] hover:-translate-y-0.5 hover:shadow-[0_12px_22px_rgba(0,0,0,0.2)] ${motionClass}`,
+    `btn-text !font-normal w-auto h-[32px] bg-pr_y text-pr_dg rounded-full px-3 py-1 shadow-[0_8px_16px_rgba(0,0,0,0.16)] hover:-translate-y-0.5 hover:shadow-[0_12px_22px_rgba(0,0,0,0.2)] ${motionClass}`,
   contact:
     `btn-text w-full h-[48px] rounded-2xl bg-pr_dg text-pr_w font-medium shadow-[0_9px_18px_rgba(0,0,0,0.18)] hover:bg-[#021f15] hover:-translate-y-0.5 hover:shadow-[0_14px_24px_rgba(0,0,0,0.22)] focus:outline-none focus:ring-2 focus:ring-pr_y ${motionClass}`,
 };
